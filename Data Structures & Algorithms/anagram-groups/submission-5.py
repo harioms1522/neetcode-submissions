cc325c031054cf1f23da889df3414c1df1fd196e
Hariom Sharma <105153233+harioms1522@users.noreply.github.com>
2026-05-21 01:32:47 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # no str sorting solution
+
+        # important no need to assign a list if key not present
+        counter_map = defaultdict(list) 
+        
+        # here the key will be an array[26]
+        # and this key will represent strings correctly
+        for s in strs:
+            counter_key = [0]*26
+            for c in s:
+                counter_key[ord(c)-ord("a")] += 1
+
+            # since dict keys should be immutable (hashable)
+            counter_map[tuple(counter_key)].append(s)
+        return list(counter_map.values())
